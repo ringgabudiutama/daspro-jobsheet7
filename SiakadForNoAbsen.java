4f20d7d41fb35c1aef45cb0cf38e052a35d0fddd
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-10-21 07:35:13 +0700

    Tugas1

---
+++ SiakadForNoAbsen.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SiakadForNoAbsen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+
+        // Menambahkan Perulangan untuk 10 mahasiswa
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+
+            // Cek nilai tertinggi
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+
+            // MengeCek nilai terendah
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+
+        // Menampilkan hasil
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+
+        sc.close();
+    }
+}
+
+    
+
