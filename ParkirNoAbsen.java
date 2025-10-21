31354c507d6a74b4abbf2c52e27e38a91ffa91ee
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-10-21 07:58:14 +0700

    Task2

---
+++ ParkirNoAbsen.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class ParkirNoAbsen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jenis, durasi;
+        int total = 0;
+
+        do {
+            System.out.print("Masukkan jenis kendaraan (1=Mobil, 2=Motor, 0=Keluar): ");
+            jenis = sc.nextInt();
+
+            if (jenis == 0) {
+                break;
+            }
+
+            if (jenis != 1 && jenis != 2) {
+                System.out.println("Jenis kendaraan tidak valid!\n");
+                continue;
+            }
+
+            System.out.print("Masukkan durasi parkir (jam): ");
+            durasi = sc.nextInt();
+
+            if (durasi > 5) {
+                total += 12500;
+            } else if (jenis == 1) {
+                total += durasi * 3000;
+            } else if (jenis == 2) {
+                total += durasi * 2000;
+            }
+
+            System.out.println("Subtotal saat ini: Rp " + total + "\n");
+
+        } while (jenis != 0);
+
+        System.out.println("Total pembayaran parkir hari ini: Rp " + total);
+        
+    }
+}
