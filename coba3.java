22506860fabbef7809c0c5b110618999a68dc7ff
Ringga Budi Utama <ringgabudiutama819@gmail.com>
2025-10-25 09:28:24 +0700

    coba3

---
+++ coba3.java
@@ -0,0 +1,9 @@
+public class coba3 {
+    public static void main(String[] args) {
+        // Cetak hasil kuadrat dari angka 1 sampai 10
+        for (int i = 1; i <= 10; i++) {
+            int kuadrat = i * i; // menghitung kuadrat
+            System.out.print(kuadrat + " ");
+        }
+    }
+}
